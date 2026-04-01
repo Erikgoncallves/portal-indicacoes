30e941ec03c5722e8d79569eee7e2865788e13b6
Erikgoncallves <75135119+Erikgoncallves@users.noreply.github.com>
2026-04-01 09:39:07 -0300

    Update sheet ID to use a new URL

--- apps-script.js
+++ apps-script.js
@@ -5,7 +5,7 @@ function doGet(e) {
 
 function doPost(e) {
   Logger.log('doPost called');
-  var sheet = SpreadsheetApp.openById('1AEIevY7Kfe4vuCTW49ZjVuYownnUfeU0uNo-Lb65ebM').getActiveSheet();
+  var sheet = SpreadsheetApp.openById('https://script.google.com/macros/s/AKfycbzyEpzhnP4L9eAOygzVyj_863KKidWX9CpfEsgr87wNm8Fctk_eCdKiLA4r5Zi0t_4Vkw/exec').getActiveSheet();
   // Handle post request logic here.
   return ContentService.createTextOutput('POST request received');
 }
